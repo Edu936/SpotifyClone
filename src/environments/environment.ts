040ba6ee7ac4c8afd731889cbaa781df5f080ff9
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2024-12-27 17:53:20 -0300

    criado as primeiras variaveis de cor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,13 @@ export const spotifyConfiguration = {
   authEndpoint: 'https://accounts.spotify.com/authorize',
   redirectUr: 'http://localhot:4200/login',
   scopes: [
-    'user-read-currently-playing', // musica tocando agora.
-    'user-read-recently-played', // ler musicas tocadas recentemente.
-    'user-read-playback-state', // ler estado do player do usuario.
-    'user-top-read', // top artistas e musicas do usuario.
-    'user-modify-playback-state', // alterar do player do usuario.
-    'user-library-read', // ler biblioteca dos usuario.
-    'playlist-read-private', // ler playlists privadas.
-    'playlist-read-collaborative', // ler playlists colaborativas.
+    'user-read-currently-playing',      // musica tocando agora.
+    'user-read-recently-played',        // ler musicas tocadas recentemente.
+    'user-read-playback-state',         // ler estado do player do usuario.
+    'user-top-read',                    // top artistas e musicas do usuario.
+    'user-modify-playback-state',       // alterar do player do usuario.
+    'user-library-read',                // ler biblioteca dos usuario.
+    'playlist-read-private',            // ler playlists privadas.
+    'playlist-read-collaborative',      // ler playlists colaborativas.
   ],
 };
