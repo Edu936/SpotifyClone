d421505926da0a7f31bd991ae28649e87fe0a4b5
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2024-12-27 17:23:12 -0300

    iniciado o projeto de clonagem spotify

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+};
+
+export const spotifyConfiguration = {
+  clientId: '6281a482670a43c69c01e5737ca900e6',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUr: 'http://localhot:4200/login',
+  scopes: [
+    'user-read-currently-playing', // musica tocando agora.
+    'user-read-recently-played', // ler musicas tocadas recentemente.
+    'user-read-playback-state', // ler estado do player do usuario.
+    'user-top-read', // top artistas e musicas do usuario.
+    'user-modify-playback-state', // alterar do player do usuario.
+    'user-library-read', // ler biblioteca dos usuario.
+    'playlist-read-private', // ler playlists privadas.
+    'playlist-read-collaborative', // ler playlists colaborativas.
+  ],
+};
