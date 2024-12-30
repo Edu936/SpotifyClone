2457d9d1e751296119932186e18330eb778e824d
Eduardo Vieira <167880647+Edu936@users.noreply.github.com>
2024-12-29 21:30:37 -0300

    criado button Menu

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -1,13 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { PlayerComponent } from './player.component';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PlayerRouter } from './player.routes';
+
+// Components 
+import { PlayerComponent } from './player.component';
+import { ButtonMenuComponent } from 'src/app/components/button-menu/button-menu.component';
 import { PaineEsquerdoComponent } from 'src/app/components/paine-esquerdo/paine-esquerdo.component';
 
 @NgModule({
   declarations: [
     PlayerComponent,
+    ButtonMenuComponent,
     PaineEsquerdoComponent,
   ],
   imports: [
